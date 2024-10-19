373babcc467b6e364554106c8062a33e437239c0
Inshal777 <inshalsikandar4@gmail.com>
2024-10-19 20:54:06 +0500

    Add main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ print("you're my sweetheart")
 print("even thou you're my entire peaceful world")
 print("we will always be togather no matter what")
 print("And i know you'll never forget me")
+Name = "Jaana"
+Age = 22
+Profession = "Medicine"
+Price = 9507474585695.210000
+print(Name, Age , Profession, Price)
