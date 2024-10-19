184811a29e8c038152dadc284181727ea168b9d0
Inshal777 <inshalsikandar4@gmail.com>
2024-10-19 23:12:47 +0500

    Add main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 print("hello world")
 print("Jaana")
 print("i missed you")
-print("i can't lost you")
+print("i can't lose you")
 print("you're everything for me")
 print("you're my sweetheart")
 print("even thou you're my entire peaceful world")
 print("we will always be togather no matter what")
 print("And i know you'll never forget me")
 Name = "Jaana"
-Age = 22
+Age = 21
 Profession = "Medicine"
 Price = 9507474585695.210000
 print(Name, Age , Profession, Price)
+Mood_Off = "JUICE PEELA DO MAUSAMI KA"
+Reaction = "MERA MOOD OFF HA"
+Daily_Glasses = 1, 2, 3, 4
+Energy_Level = 99.999999
